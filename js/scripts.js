35953651b4cc8655ff25b127ee23975c5897d53c
Suryansh <shahisuryansh28@gmail.com>
2021-09-28 18:33:00 +0530

    Adding project

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    //    $('[data-toggle="tooltip"]').tooltip();
+        $('#mycarousel').carousel({interval: 2000});
+        $('#carouselButton').click(function() {
+            if($('#carouselButton').children('span').hasClass('fa-pause'))
+            {
+                $('#mycarousel').carousel('pause');
+                $('#carouselButton').children('span').removeClass('fa-pause');
+                $('#carouselButton').children('span').addClass('fa-play');
+            }
+            else
+            {
+                $('#mycarousel').carousel('cycle');
+                $('#carouselButton').children('span').removeClass('fa-play');
+                $('#carouselButton').children('span').addClass('fa-pause');
+            }    
+        });
+
+        $('.login').click(function() {
+            $('#loginModal').modal('show');
+        });
+
+        $('.reserveTable').click(function() {
+            $('#reserveTable').modal('show');
+        });
+        
+   })
